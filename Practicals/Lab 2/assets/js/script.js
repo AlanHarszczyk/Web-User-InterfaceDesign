395147dd4222e4d69f5f4afb3c91c47493636480
(Student  C00285418) Alan Harszczyk <C00285418@setu.ie>
2024-01-30 11:27:36 +0000

    copied lab 1 for startup

---
+++ Practicals/Lab 2/assets/js/script.js
@@ -0,0 +1,14 @@
+window.onload = function()
+{
+    alert('Thank you for visting and supporting our efforts');
+};
+
+document.querySelector('header').addEventListener('click',function()
+{
+   this.style.color = 'black';
+});
+
+
+
+
+
