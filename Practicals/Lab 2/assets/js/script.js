a2411b0e64feb3807dcf2d6b9a17f9519db087f1
(Student  C00285418) Alan Harszczyk <C00285418@setu.ie>
2024-02-02 12:07:17 +0000

    form fin, still need to do table

--- Practicals/Lab 2/assets/js/script.js
+++ Practicals/Lab 2/assets/js/script.js
@@ -1,3 +1,4 @@
+/*pop-up text*/
 window.onload = function()
 {
     alert('Thank you for visting and supporting our efforts');
@@ -8,7 +9,19 @@ document.querySelector('header').addEventListener('click',function()
    this.style.color = 'black';
 });
 
+/*empty text*/
+if(document.getElementById('text-input').value === ' ')
+{
+    alert('Text input cannot be empty!');
+}
 
+/*check for email*/
+var email = document.getElementById('email-input').value;
+var pattern = /^[^ ]+@[^ ]+\[a-z]{2,3}$/;
+if (!email.match(pattern)) 
+{
+    alert('Please enter a valid email address!');
+}
 
 
 
