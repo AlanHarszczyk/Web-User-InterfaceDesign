920af5feeb097be185e8e462c3eb00cf1501ac8a
(Student  C00285418) Alan Harszczyk <c00285418@setu.ie>
2024-02-06 11:42:01 +0000

    beagan 3 plus started about section

---
+++ Practicals/Lab 3/assets/js/script.js
@@ -0,0 +1,25 @@
+/*pop-up text*/
+window.onload = function()
+{
+    alert('Thank you for visting and supporting our efforts');
+};
+
+document.querySelector('header').addEventListener('click',function()
+{
+   this.style.color = 'black';
+});
+
+/*empty text*/
+if(document.getElementById('text-input').value === ' ')
+{
+    alert('Text input cannot be empty!');
+}
+
+/*check for email*/
+var email = document.getElementById('email-input').value;
+var pattern = /^[^ ]+@[^ ]+\[a-z]{2,3}$/;
+if (!email.match(pattern)) 
+{
+    alert('Please enter a valid email address!');
+}
+
