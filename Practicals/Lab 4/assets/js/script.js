ef6bb4ff3843e14872daa96d434402cac767a070
(Student  C00285418) Alan Harszczyk <c00285418@setu.ie>
2024-02-15 11:58:38 +0000

    donation page started on lab 4

---
+++ Practicals/Lab 4/assets/js/script.js
@@ -0,0 +1,49 @@
+/*pop-up text*/
+window.onload = function()
+{
+    alert('Thank you for visting and supporting our efforts');
+};
+
+function datasaver()
+{
+
+let username = document.getElementById("username").value;
+let surname = document.getElementById("surname").value;
+let email = document.getElementById("email").value;
+let phonenumber = document.getElementById("phonenumber").value;
+
+    alert("Your data is being saved");
+console.log(document.getElementById("username").value)
+console.log(document.getElementById("surname").value)
+console.log(document.getElementById("email").value)
+console.log(document.getElementById("phonenumber").value)
+
+  localStorage.setItem("username" , username);
+  localStorage.setItem("surname" , surname);
+  localStorage.setItem("email" , email);
+  localStorage.setItem("phonenumber" , phonenumber);
+  
+}
+
+function retrieveData()
+{
+  
+    if(localStorage.getItem("username")!="")
+    {
+        let username = localStorage.getItem("username");
+        let surname = localStorage.getItem("surname");
+        let email= localStorage.getItem("email");
+        let phonenumer = localStorage.getItem("phonenumber");
+        console.log("username" + username);
+        console.log("surname" + surname);
+        console.log("email" + email);
+        console.log("phonenumber" + phonenumer);
+    }
+
+    else
+    {
+        alert('Please enter valid details!');
+    }
+
+}
+
